3c3577729e93aa9234fac47c930cef0e4c7a7232
lany9527 <lany9527@gmail.com>
2017-02-15 23:30:05 +0800

    添加组件1

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+/**
+ * Created by littlestone on 2017/2/15.
+ */
+import React from 'react'
+import Footer from './Footer'
+import AddTodo from '../containers/AddTodo'
+import VisibleTodoList from '../containers/VisibleTodoList'
+
+const App = () => (
+  <div>
+    <AddTodo />
+    <VisibleTodoList />
+    <Footer />
+  </div>
+)
+
+export default App
---
+++ src/components/TodoList.js
@@ -0,0 +1,28 @@
+/**
+ * Created by littlestone on 2017/2/15.
+ */
+import React, {PropTypes} from 'react';
+import Todo from './Todoo';
+
+const TodoList = ({todos, onTodoClick}) => (
+  <ul>
+    {todos.map(todo =>
+      <Todo
+        key={todo.id}
+        {...todo}
+        onClick={()=>onTodoClick(todo.id)}
+      />
+    )}
+  </ul>
+);
+
+TodoList.propTypes = {
+  todos: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      completed:PropTypes.bool.isRequired,
+      text: PropTypes.string.isRequired
+    }).isRequired
+  ).isRequired
+};
+export default TodoList;
